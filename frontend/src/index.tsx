bc2285886143a8b0b54e5ba7548c5a005d682878
kiudev <dani.97.saavedra@gmail.com>
2024-09-20 19:01:55 +0200

    wrap search provider

--- frontend/src/index.tsx
+++ frontend/src/index.tsx
@@ -1,13 +1,16 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import App from './App';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App";
+import SearchContextProvider from "./context/searchContext";
 
-const rootEl = document.getElementById('root');
+const rootEl = document.getElementById("root");
 if (rootEl) {
   const root = ReactDOM.createRoot(rootEl);
   root.render(
     <React.StrictMode>
-      <App />
-    </React.StrictMode>,
+      <SearchContextProvider>
+        <App />
+      </SearchContextProvider>
+    </React.StrictMode>
   );
 }
