a5d5fb291cafed4d1ee84061e9584d840f94299f
kiudev <dani.97.saavedra@gmail.com>
2024-09-29 18:22:31 +0200

    export types

---
+++ frontend/src/types.ts
@@ -0,0 +1,59 @@
+import { ChangeEvent, RefObject } from "react";
+
+export interface SearchContextType {
+  searchValue: string;
+  handleSearchChange: (e: ChangeEvent<HTMLInputElement>) => void;
+  photosRef: RefObject<HTMLDivElement>;
+  photos: any[];
+  searchPhotos: (query: string) => void;
+}
+
+export interface PhotoContextType {
+  selectedPhoto: {
+    id: number;
+    src: {
+      original: string;
+      large2x: string;
+      large: string;
+      medium: string;
+      small: string;
+      portrait: string;
+      landscape: string;
+      tiny: string;
+    };
+    alt: string;
+    width: number;
+    height: number;
+  } | null;
+  handlePhotoClick: (id: number) => void;
+  setSelectedPhoto: (photo: {
+    id: number;
+    src: {
+      original: string;
+      large2x: string;
+      large: string;
+      medium: string;
+      small: string;
+      portrait: string;
+      landscape: string;
+      tiny: string;
+    };
+    alt: string;
+    width: number;
+    height: number;
+  } |  null) => void;
+
+}
+
+export interface PhotoType {
+  id: number;
+  src: {
+    original: string;
+    large: string;
+  };
+  alt: string;
+}
+
+export interface FormProps {
+  onSearch?: (query: string) => void;
+}
