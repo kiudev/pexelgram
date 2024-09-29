f75d93bbc72a24e76a6c5e20e53372ace40210bc
kiudev <dani.97.saavedra@gmail.com>
2024-09-29 09:10:36 +0200

    add new context

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -5,12 +5,15 @@ import PhotosLayout from "./layout/PhotosLayout";
 
 // context
 import SearchContextProvider from "./context/SearchContext";
+import PhotoProvider from "./context/PhotoContext";
 
 export default function App() {
   return (
     <div className="min-w-screen min-h-screen bg-mixed-100 flex flex-col px-[15vw]">
       <SearchContextProvider>
-        <PhotosLayout />
+        <PhotoProvider>
+          <PhotosLayout />
+        </PhotoProvider>
       </SearchContextProvider>
     </div>
   );
