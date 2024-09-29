711a3ba76819b3aa8bcf1df39cbf041478d518e8
kiudev <dani.97.saavedra@gmail.com>
2024-09-29 09:12:16 +0200

    add lazy load image component

--- frontend/src/components/PhotoItem.tsx
+++ frontend/src/components/PhotoItem.tsx
@@ -1,14 +1,35 @@
 import DownloadButton from "./DownloadButton";
+import { LazyLoadImage } from "react-lazy-load-image-component";
+import Skeleton from "react-loading-skeleton";
+import "react-loading-skeleton/dist/skeleton.css";
+import { useContext } from "react";
+import { PhotoContext } from "../context/PhotoContext";
+
+function usePhotoContext() {
+  const photoContext = useContext(PhotoContext);
+  if (photoContext === null) {
+    throw new Error('usePhotoContext must be used within a PhotoProvider');
+  }
+  return photoContext;
+}
 
 export default function PhotoItem({ photo }: any) {
+  const { handlePhotoClick } = usePhotoContext();
+
   return (
     <div className="relative cursor-pointer" key={photo.id}>
-      <img
-        className="photos-gallery mb-3 rounded-lg hover:opacity-10"
+      <LazyLoadImage
+        onClick={() => handlePhotoClick(photo.id)}
+        className="photos-gallery mb-3 rounded-lg"
         src={photo.src.large}
         alt={photo.alt}
+        width={500}
+        height={500}
       />
-      <DownloadButton url={photo.src.original} fileName={photo.alt.replace(/ /g, '_')} />
+      <DownloadButton
+        url={photo.src.original}
+        fileName={photo.alt.replace(/ /g, "_")}
+      />
     </div>
   );
 }
