48a3b488f4517dceeda6d0dc5555a619d39d509e
kiudev <dani.97.saavedra@gmail.com>
2024-10-06 19:57:02 +0200

    install cors

--- backend/index.ts
+++ backend/index.ts
@@ -1,28 +1,13 @@
 import "dotenv/config";
 import express from "express";
 import photosRouter from "./routes/photos.js";
+import cors from "cors";
 
 const app = express();
 const PORT = process.env.PEXELGRAM_PORT;
 
 app.use(express.json());
-app.use((req, res, next) => {
-  res.header("Access-Control-Allow-Origin", "http://localhost:3000");
-  res.header("Access-Control-Allow-Credentials", "true");
-  res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
-  res.header(
-    "Access-Control-Allow-Headers",
-    "Origin, X-Requested-With, Content-Type, Accept, Authorization"
-  );
-  res.header("Vary", "Origin");
-
-  // Handle preflight
-  if (req.method === "OPTIONS") {
-    return res.sendStatus(204);
-  }
-
-  next();
-});
+app.use(cors());
 
 app.use("/v1/photos", photosRouter);
 
